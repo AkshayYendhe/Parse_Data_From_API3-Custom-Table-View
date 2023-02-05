d0c2c3b96bb6d4a735d6c25222633bb8c46f35b1
forloop <yendheakshayd@gmail.com>
2023-02-05 11:22:10 +0530

    Initial Commit

---
+++ Parse_Data_From_API3 Custom Table View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parse_Data_From_API3 Custom Table View
+//
+//  Created by Akshay Yendhe on 05/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
